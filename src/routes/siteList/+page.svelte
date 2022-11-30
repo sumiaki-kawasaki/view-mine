3b3df65a97f2ae799520c6076e5d93d244eef23d
川崎澄明 <cb_clear_bright@yahoo.co.jp>
2022-11-30 14:33:55 +0000

    SiteListの登録・削除を暫定実装

--- src/routes/siteList/+page.svelte
+++ src/routes/siteList/+page.svelte
@@ -1,5 +1,16 @@
-<script>
+<script lang="ts">
 	import { sites } from '$lib/local-storage-store';
+  import RegistSiteDialog from './RegistSiteDialog.svelte';
+
+  let isShowRegistDialog = false;
+  const ShowRegistDialog = () => isShowRegistDialog = true;
+  const CloseRegistDialog = () => isShowRegistDialog = false;
+  const DeleteSite = (idx: number) => {
+    sites.remove(idx);
+  };
+  const ChangeSite = (idx: number) => {
+    // TODO
+  };
 </script>
 
 <svelte:head>
@@ -9,17 +20,26 @@
 
 <h1>登録Redmine情報</h1>
 <div>
+  <button type="button" class="btn" on:click={ShowRegistDialog}>Redmine追加</button>
   {#if $sites.length > 0}
     <ul class="site-list">
-      {#each $sites as site }
+      {#each $sites as site, i}
         <li>
-          <p>site.name</p>
-          <p>site.url</p>
-          <p>site.key</p>
-        </li>			
+          <p>{site.name}</p>
+          <p>{site.url}</p>
+          <p>{site.key}</p>
+          <div class="buttons">
+            <button class="btn" on:click={() => ChangeSite(i)}>変更</button>
+            <button class="btn" on:click={() => DeleteSite(i)}>削除</button>
+          </div>
+        </li>
       {/each}
     </ul>
   {:else}
     <p>登録は0件です</p>
   {/if}
 </div>
+
+{#if isShowRegistDialog}
+  <RegistSiteDialog on:close={CloseRegistDialog} />
+{/if}
---
+++ src/routes/siteList/RegistSiteDialog.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+	import { sites } from '$lib/local-storage-store';
+
+  const dispatch = createEventDispatcher();
+  let registName = "";
+  let registUrl = "";
+  let registKey = "";
+
+  const CloseDialog = () => { 
+    dispatch("close");
+  };
+  const RegistSite = () => {
+    if (!Varidate()) { return; }
+
+    sites.add({
+      name: registName,
+      url: registUrl,
+      key: registKey
+    });
+    CloseDialog();
+  };
+  const Varidate = () => {
+    if (registName == "") {
+      alert("登録名が空欄です");
+      return false;
+    }
+    if (registUrl == "") {
+      alert("URLが空欄です");
+      return false;
+    }
+    if (registKey == "") {
+      alert("API Keyが空欄です");
+      return false;
+    }
+    return true;
+  };
+</script>
+
+<div class="modal">
+  <div class="modal-background">
+    <div class="modal-contents">
+      <span class="close" on:click={CloseDialog}>x</span>
+      <h2>Redmine情報登録</h2>
+      <div class="contents">
+        <label class="input-name">
+          <p>登録名</p>
+          <input bind:value={registName} type="text" />
+        </label>
+        <label class="input-url">
+          <p>URL</p>
+          <input bind:value={registUrl} type="text" />
+        </label>
+        <label class="input-key">
+          <p>API Key</p>
+          <input bind:value={registKey} type="text" />
+        </label>
+        <div class="buttons">
+          <button class="btn" on:click={CloseDialog}>キャンセル</button>
+          <button class="btn" on:click={RegistSite}>登録</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
